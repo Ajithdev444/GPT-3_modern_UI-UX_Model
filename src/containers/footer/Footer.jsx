093abb2f6fd993603debf98d79e1c971adfbb648
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-19 22:58:25 +0530

    Blog and Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to
+        step in to future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request  Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>GPT3 ,All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Contact us</p>
+          <p>Social Media</p>
+          <p>Feedbacks</p>
+          <p>Our Networks</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>GPT Org</p>
+          <p>+91 525458525</p>
+          <p>gpt@gmail.com</p>
+          <p>gptCompany.in</p>
+        </div>
+      </div>
+
+      <div className='gpt3__footer-copyright'>
+        <p>2022 Ajithdev C. All Rights Reserved</p> 
+      </div>
+
+    </div>
   )
 }
 
